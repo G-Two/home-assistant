7e88113a42f658eeedca076c96c3e1f9a96f4207
G-Two <7310260+G-Two@users.noreply.github.com>
2021-07-10 17:49:57 +0000

    Change EV time remaining sensor to ISO format
    - Bump to subarulink 0.3.14

--- homeassistant/components/subaru/sensor.py
+++ homeassistant/components/subaru/sensor.py
@@ -13,7 +13,6 @@ from homeassistant.const import (
     PERCENTAGE,
     PRESSURE_HPA,
     TEMP_CELSIUS,
-    TIME_MINUTES,
     VOLT,
     VOLUME_GALLONS,
     VOLUME_LITERS,
@@ -132,8 +131,8 @@ EV_SENSORS = [
     {
         SENSOR_TYPE: "EV Time to Full Charge",
         SENSOR_CLASS: DEVICE_CLASS_TIMESTAMP,
-        SENSOR_FIELD: sc.EV_TIME_TO_FULLY_CHARGED,
-        SENSOR_UNITS: TIME_MINUTES,
+        SENSOR_FIELD: sc.EV_TIME_TO_FULLY_CHARGED_UTC,
+        SENSOR_UNITS: None,
     },
 ]
 
@@ -264,16 +263,22 @@ class SubaruSensor(SubaruEntity, SensorEntity):
         last_update_success = super().available
         if last_update_success and self.vin not in self.coordinator.data:
             return False
+        elif self.state is None:
+            return False
         return last_update_success
 
     def get_current_value(self):
         """Get raw value from the coordinator."""
-        value = self.coordinator.data[self.vin][VEHICLE_STATUS].get(self.data_field)
-        if value in sc.BAD_SENSOR_VALUES:
-            value = None
-        if isinstance(value, str):
-            if "." in value:
-                value = float(value)
-            else:
-                value = int(value)
-        return value
+        if type(self.coordinator.data) == dict:
+            value = self.coordinator.data.get(self.vin)[VEHICLE_STATUS].get(
+                self.data_field
+            )
+            if value in sc.BAD_SENSOR_VALUES:
+                value = None
+            if isinstance(value, str):
+                if "." in value:
+                    value = float(value)
+                elif value.isdigit():
+                    value = int(value)
+            return value
+        return None
